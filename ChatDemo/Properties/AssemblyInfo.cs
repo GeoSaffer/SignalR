7dd6fce8bfc21aa601fde6b996ee167e305ee26f
GeoSaffer <dfouche8@gmail.com>
2019-05-23 22:05:34 +1200

    update some style sheets and layouts

--- ChatDemo/Properties/AssemblyInfo.cs
+++ ChatDemo/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.7069.1780")]
+[assembly: AssemblyVersion("1.0.7082.17835")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- ClientViewer/Properties/AssemblyInfo.cs
+++ ClientViewer/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.7069.6538")]
+[assembly: AssemblyVersion("1.0.7082.15688")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
